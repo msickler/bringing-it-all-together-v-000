ae4fe4a5554c9e9e2b27b11e54a93b6cae7a427f
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-04 18:16:02 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -23,10 +23,10 @@ class Dog
     DB[:conn].execute(sql)
   end
 
-  def save 
-    if self.id 
-      self.update 
-    else 
+  def save
+    if self.id
+      self.update
+    else
       sql = "INSERT INTO dogs (name, breed) VALUES (?, ?)"
       DB[:conn].execute(sql, self.name, self.album)
     end
